9800a8f2b1d565bcdfed7b12cb0cddc2c53e746a
Oğuzhan Kılıç <41840875+ouzkilic@users.noreply.github.com>
2023-02-11 20:05:52 +0300

    Materialui react (#8) (#9)
    * material ui ve css fixes
    
    * hot-fix
    
    Co-authored-by: cagan123-alt <85545585+cagan123-alt@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import useFetch from "./components/useFetch";
 import MainViewContaier from "./components/MainViewContainer";
 
 export default function App() {
-  const { data, loading, error } = useFetch(
-    "https://eczaneapi.afetharita.com/api/cityWithDistricts "
-  );
-  if (loading) return <h1>Yükleniyor</h1>;
-  if (error) console.log(error);
-
+ 
   return <MainViewContaier />;
 }
