094a5d2d0da17f13d39a01493ab6cef8529086f7
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2023-02-12 18:33:36 +0300

    Footer işlevlik ve styling (#51)
    fixes #14

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,129 @@
+import React from "react";
+import styled from "styled-components";
+import { BREAKPOINTS } from "../../utils/styled";
+import "./style.css";
+
+const SIconWrapper = styled.div`
+  display: none;
+  align-items: center;
+`;
+
+const SLeftIcon = styled.img`
+  height: 0.75rem;
+  width: 0.75rem;
+  @media ${BREAKPOINTS.MD.min} {
+    height: 2rem;
+    width: 2rem;
+  }
+  margin: 0 0.5rem;
+`;
+
+const SButton = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+`;
+
+const SRightIcon = styled(SLeftIcon)`
+  transform: rotate(180deg);
+`;
+
+const SParagWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SParag = styled.p`
+  font-family: "SegoeUI";
+  font-size: 0.75rem;
+  color: white;
+  @media ${BREAKPOINTS.MD.min} {
+    font-size: 2.25rem;
+  }
+`;
+
+export function Footer({
+  cityData,
+  selectedCity,
+  handleChangeCity,
+  selectedDist,
+  setSelectedDist,
+  allData,
+}) {
+  const noCitySelected = !selectedCity;
+
+  const selectedCityData = cityData?.data?.find((a) => a.key === selectedCity);
+
+  const selectedCityDistricts = selectedCityData?.districts;
+
+  const allCities = cityData?.data?.map((item) => item.key);
+
+  const cityDistrictWithData = selectedCityDistricts
+    ?.filter((dist) => {
+      if (!selectedCityDistricts) return true;
+      const distData = allData.find((d) => d.district === dist.key);
+      return !!distData;
+    })
+    .map((i) => i.key);
+
+  return (
+    <div className="footer-container">
+      <div className="ilce-box">
+        <SIconWrapper>
+          <SButton>
+            <SLeftIcon src="/left-icon.svg" />
+          </SButton>
+        </SIconWrapper>
+        <div className="ilce-items">
+          {noCitySelected && (
+            <SParagWrapper>
+              <SParag>Şehir Seçiniz</SParag>
+            </SParagWrapper>
+          )}
+          {selectedCityDistricts?.map((item) => (
+            <button
+              className={
+                cityDistrictWithData.indexOf(item.key) === -1
+                  ? "ilce-item ilce-disabled"
+                  : selectedDist === item.key
+                  ? "ilce-item ilce-active"
+                  : "ilce-item"
+              }
+              onClick={() => {
+                setSelectedDist(item.key);
+              }}
+              key={item.id}
+              disabled={cityDistrictWithData.indexOf(item.key) === -1}
+            >
+              {item.key}
+            </button>
+          ))}
+        </div>
+        <SIconWrapper>
+          <SButton>
+            <SRightIcon src="/left-icon.svg" />
+          </SButton>
+        </SIconWrapper>
+        s
+      </div>
+
+      <div className="cities-box">
+        {allCities?.map((item) => (
+          <button
+            className={
+              selectedCity === item ? "city-item city-item-active" : "city-item"
+            }
+            key={item}
+            onClick={() => handleChangeCity(item)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/MainViewContainer.js
+++ src/components/MainViewContainer.js
@@ -1,11 +1,14 @@
-import { Box, Button, Grid, Paper, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import axios from "axios";
 import L from "leaflet";
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import styled from "styled-components";
+import { BREAKPOINTS } from "../utils/styled";
 import centers from "./cityCenters";
 import DownButton from "./DownButton";
+import { Footer } from "./Footer/Footer";
 import { HeaderCombined } from "./Header/HeaderCombined";
 import { FILTER, SEARCH_AT } from "./Header/HeaderRow";
 import InfoCard from "./InfoCard";
@@ -18,6 +21,17 @@ import pharmacyIcon2Svg from '../icons/pharmacy-marker-2.png'
 
 
 
+const SPaper = styled.div`
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.87);
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  background-color: #182151;
+  margin-bottom: 1.5rem;
+  @media ${BREAKPOINTS.MD.min} {
+    margin-bottom: 3rem;
+  }
+`;
+
 const MainViewContaier = () => {
   const [visible, setVisible] = useState(false);
   const [mapRef, setMapRef] = React.useState();
@@ -26,8 +40,9 @@ const MainViewContaier = () => {
   const [filter, setFilter] = useState(FILTER.HEPSI);
   const [searchBarVal, setSearchbarVal] = useState("");
   const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedDist, setSelectedDist] = useState(null);
 
-  const [citydata, setCityData] = React.useState(null);
+  const [cityData, setCityData] = React.useState(null);
 
   const [allData, setAllData] = React.useState(null);
 
@@ -110,6 +125,7 @@ const MainViewContaier = () => {
     const lng = centers[city]?.lng;
     mapRef.flyTo([lat, lng], 12);
     setSelectedCity(city);
+    setSelectedDist(null);
   };
 
   useEffect(() => {
@@ -163,12 +179,15 @@ const MainViewContaier = () => {
           (item) => item.city.toLowerCase() === selectedCity.toLowerCase()
         );
 
+  const distFilteredData =
+    selectedDist == null
+      ? cityFilteredData
+      : cityFilteredData?.filter(
+          (item) => item.district.toLowerCase() === selectedDist.toLowerCase()
+        );
+
   return (
-    <Paper
-      id="fullheight"
-      sx={{ bgcolor: "#182151", height: "100%", padding: "0px" }}
-      variant="outlined"
-    >
+    <SPaper>
       <UpButton visible={visible}></UpButton>
       <DownButton visible={!visible}></DownButton>
       <HeaderCombined
@@ -225,48 +244,17 @@ const MainViewContaier = () => {
           </MapContainer>
         </Box>
       )}
-      {searchAt === SEARCH_AT.LISTE && <ListPage data={cityFilteredData} />}
+      {searchAt === SEARCH_AT.LISTE && <ListPage data={distFilteredData} />}
 
-      <Box
-        sx={{
-          flexGrow: 1,
-          marginTop: "30px",
-          height: "auto",
-          overflow: "auto",
-          textAlign: "center",
-        }}
-      >
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 12, md: 12 }}
-        >
-          {citydata?.data.map((city, index) => (
-            <Grid item xs={2} sm={4} md={3} key={index}>
-              <Button
-                onClick={() => handleChangeCity(city.key)}
-                sx={{
-                  color: "white",
-                  border: "solid 0.5px",
-                  height: "50px",
-                  width: "150px",
-                }}
-                variant="outlined"
-              >
-                <span
-                  style={{
-                    display: "block",
-                    overflow: "hidden",
-                    whiteSpace: "nowrap",
-                    textOverflow: "ellipsis",
-                  }}
-                >{`${city.key}`}</span>
-              </Button>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </Paper>
+      <Footer
+        cityData={cityData}
+        selectedCity={selectedCity}
+        handleChangeCity={handleChangeCity}
+        selectedDist={selectedDist}
+        setSelectedDist={setSelectedDist}
+        allData={allData}
+      />
+    </SPaper>
   );
 };
 export default MainViewContaier;
