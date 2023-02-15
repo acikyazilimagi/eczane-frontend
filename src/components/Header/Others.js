1ca1ebb5d13e884a7fd299b2e828eebc58670e87
Mehmet Kürşat Aydın <kursat.aydin@hangikredi.com>
2023-02-16 02:03:33 +0300

    Converted Others styled

--- src/components/Header/Others.js
+++ src/components/Header/Others.js
@@ -1,96 +1,9 @@
-import styled from "@emotion/styled";
 import React, { useState } from "react";
 import haritaLogo from "../../icons/afet-harita.svg";
 import ioLogo from "../../icons/deprem-io.svg";
 import yardimLogo from "../../icons/deprem-yardim.svg";
-import { BREAKPOINTS } from "../../utils/styled";
 import { useWindowSize } from "../../utils/hooks";
-
-const SOtherWrapper = styled.div`
-  display: flex;
-  position: relative;
-  align-items: center;
-  gap: 10px;
-`;
-
-const SOtherTitle = styled.h4`
-  font-family: "Roboto", sans-serif;
-  text-align: center;
-  color: white;
-  font-size: 1.5rem;
-  font-weight: 500;
-  white-space: nowrap;
-  display: none;
-
-  @media ${BREAKPOINTS.LG.min} {
-    display: block;
-  }
-`;
-
-const SLogoContainer = styled.div`
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  background: rgba(255, 255, 255, 0.22);
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5.5px);
-  -webkit-backdrop-filter: blur(5.5px);
-  border-radius: 0.625rem;
-  gap: 1.25rem;
-  padding: 0.25rem 0.5rem;
-
-  @media ${BREAKPOINTS.MD.min} {
-    padding: 0.5rem 1rem;
-  }
-
-  & > a {
-    background-color: white;
-    border-radius: 50%;
-  }
-
-  & img {
-    width: 2rem;
-    padding: 5px;
-    border-radius: 50%;
-    cursor: pointer;
-    @media ${BREAKPOINTS.MD.min} {
-      width: 3.75rem;
-    }
-  }
-
-  & a:nth-child(3) {
-    padding: 0;
-  }
-
-  & a:nth-child(3) img {
-    scale: 1.1;
-  }
-
-  & a:nth-child(3) img:hover {
-    scale: 1.2;
-  }
-
-  & img:hover {
-    scale: 1.1;
-  }
-`;
-
-const SDescription = styled.div`
-  position: absolute;
-  top: 6rem;
-  right: 0rem;
-  background: white;
-  padding: 0.625rem;
-  border-radius: 0.625rem;
-  box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
-  z-index: 1;
-
-  & > p {
-    font-family: "Roboto", sans-serif;
-  }
-`;
+import styles from "./Others.module.scss";
 
 const Others = () => {
   const [isHovering, setIsHovering] = useState("");
@@ -108,9 +21,10 @@ const Others = () => {
   };
 
   return (
-    <SOtherWrapper>
-      <SOtherTitle>Yardımcı Siteler:</SOtherTitle>
-      <SLogoContainer
+    <div className={styles.otherWrapper}>
+      <h4 className={styles.otherTitle}>Yardımcı Siteler:</h4>
+      <div
+        className={styles.logoContainer}
         onMouseOver={(event) => handleMouseOver(event)}
         onMouseOut={handleMouseOut}
       >
@@ -128,24 +42,24 @@ const Others = () => {
         <a href="https://depremyardim.com/" target="_blank" rel="noreferrer">
           <img id="yardim" src={yardimLogo} alt="deprem-yardim" />
         </a>
-      </SLogoContainer>
+      </div>
       {isHovering === "harita" ? (
-        <SDescription>
+        <div className={styles.description}>
           <p>Depremzedeleri aramak ve yardım etmek için kullandığımız site.</p>
-        </SDescription>
+        </div>
       ) : isHovering === "io" ? (
-        <SDescription>
+        <div className={styles.description}>
           <p>
             Afetle ilgili çeşitli konularda bilgi almak için kullandığımız web
             sitesi.
           </p>
-        </SDescription>
+        </div>
       ) : isHovering === "yardim" ? (
-        <SDescription>
+        <div className={styles.description}>
           <p>Depremzedelere erzak yardımı için kullandığımız site.</p>
-        </SDescription>
+        </div>
       ) : null}
-    </SOtherWrapper>
+    </div>
   );
 };
 
