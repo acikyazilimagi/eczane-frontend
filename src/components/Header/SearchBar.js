3bc78ee8422473ec1c4bcb8fe7ea76276cd0c80c
Mehmet Kürşat Aydın <kursat.aydin@hangikredi.com>
2023-02-16 01:10:13 +0300

    Converted SearchBar styled

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,90 +1,25 @@
-import styled from "@emotion/styled";
-import { BREAKPOINTS } from "../../utils/styled";
-
-const SSearchBoxWrapper = styled.div`
-  position: relative;
-  flex-grow: 1;
-  @media ${BREAKPOINTS.MD.min} {
-    flex-grow: 0;
-    min-width: 350px;
-  }
-  @media ${BREAKPOINTS.LG.min} {
-    min-width: 500px;
-  }
-  @media ${BREAKPOINTS.XL.min} {
-    min-width: unset;
-    flex-grow: 1;
-  }
-`;
-
-const SInput = styled.input`
-  height: 100%;
-  border-radius: 0.625rem;
-  padding: 0.5rem;
-  outline: none;
-  border: 1px solid white;
-  width: 100%;
-  font-size: 0.625rem;
-  font-family: "Roboto", sans-serif;
-
-  &::placeholder {
-    font-family: "Roboto", sans-serif;
-    font-size: 0.625rem;
-  }
-
-  @media ${BREAKPOINTS.MD.min} {
-    font-size: 1.625rem;
-    padding: 1.25rem 1.875rem 1.25rem 1.25rem;
-    &::placeholder {
-      font-size: 1.625rem;
-    }
-  }
-`;
-
-const SSearchIcon = styled.img`
-  height: 0.75rem;
-  width: 0.75rem;
-  @media ${BREAKPOINTS.MD.min} {
-    height: 2rem;
-    width: 2rem;
-  }
-`;
-
-const SNoStyleButton = styled.button`
-  background: none;
-  color: inherit;
-  border: none;
-  padding: 0;
-  font: inherit;
-  cursor: pointer;
-  outline: inherit;
-`;
-
-const SSearchIconWrapper = styled.div`
-  position: absolute;
-  right: 0.5rem;
-  top: 50%;
-  transform: translateY(-50%);
-  @media${BREAKPOINTS.MD.min} {
-    right: 1.875rem;
-  }
-`;
+import styles from "./SearchBar.module.scss";
 
 function SearchBar({ searchBarVal, setSearchBarVal }) {
   return (
-    <SSearchBoxWrapper>
-      <SInput
+    <div className={styles.SearchBoxWrapper}>
+      <input
         type="text"
         placeholder="Ara"
         value={searchBarVal}
         onInput={(e) => setSearchBarVal(e.target.value)}
+        className={styles.Input}
       />
-      <SSearchIconWrapper>
-        <SNoStyleButton>
-          <SSearchIcon src="/search-icon.svg" />
-        </SNoStyleButton>
-      </SSearchIconWrapper>
-    </SSearchBoxWrapper>
+      <div className={styles.SearchIconWrapper}>
+        <button className={styles.NoStyleButton}>
+          <img
+            src="/search-icon.svg"
+            alt="Search Icon"
+            className={styles.SearchIcon}
+          />
+        </button>
+      </div>
+    </div>
   );
 }
 
