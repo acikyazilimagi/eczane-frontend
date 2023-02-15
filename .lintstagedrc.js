28de9d3a98040bd134782a0023879cece22398a5
eraygundogmus <eraygundogmus@teknasyon.com>
2023-02-15 04:26:42 +0300

    chore: add husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) => `npx eslint --fix --max-warnings=0`;
+
+module.exports = {
+  "./src/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*/src/.{js,jsx,ts,tsx,css,svg}": ["yarn prettier-fix"],
+};
