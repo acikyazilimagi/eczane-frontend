60392c9088550e5e35b476799e9a8fbbc10a8d90
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2023-02-22 15:24:19 +0300

    fix: add prop-types

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,13 @@ import "leaflet/dist/leaflet.css";
 import "react-leaflet-fullscreen/dist/styles.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
+import PropTypes from "prop-types";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
